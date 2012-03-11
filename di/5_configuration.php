a0f7374a764a3819450adfd31552f8e8d5f19e9b
Rob Allen <rob@akrabat.com>
2012-03-11 09:19:00 +0000

    DI configuration

--- di/5_configuration.php
+++ di/5_configuration.php
@@ -5,16 +5,22 @@ include __DIR__ . "/../load_zf.php";
 // register the My/ namespace
 $autoLoader->registerNamespace('My', __DIR__ . '/My/');
 
-$config = new Zend\Config\Ini(__DIR__ . '/5_configuration.ini', 'production');
-$diConfig = new Zend\Di\Configuration($config->di);
+$data = array (
+    'instance' => array (
+        'My\DatabaseAdapter' => array (
+            'parameters' => array (
+                'dsn' => 'mysql:dbname=ini',
+            ),
+        ),
+    ),
+);
+$diConfig = new \Zend\Di\Configuration($data);
 
-$di = new Zend\Di\DependencyInjector($diConfig);
+$di = new \Zend\Di\Di();
+$diConfig->configure($di);
+
 
 // Test
-echo PHP_EOL . 'DI: $artist = $di->get("artist");' . PHP_EOL;
-$artist = $di->get("artist");
-var_dump($artist);
-
-echo PHP_EOL . 'DI: $album = $di->get("album", array("name" => "Smile"));' . PHP_EOL;
-$album = $di->get("album", array("name" => "Queen"));
-var_dump($album);
+echo PHP_EOL . 'DI: $users = $di->get("My\UserTable");' . PHP_EOL;
+$users = $di->get("My\UserTable");
+var_dump($users);
